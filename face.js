e20520567115bcbe430a57d997d4859e54f21ffb
Uğur Şişik <44878194+Ugur340@users.noreply.github.com>
2020-10-18 17:36:02 +0300

    Add files via upload

---
+++ face.js
@@ -0,0 +1,44 @@
+function setup() {
+    createCanvas(700, 350);
+  }
+  
+  function draw() {
+    background(255);
+    fill(150,140,180);
+    ellipse(175,150,200,230);
+    triangle(85,100, 80, 160, 60, 70);
+    triangle(290,70,270,160,265,100);
+    noStroke();
+    fill(0);
+    ellipse(175,215,70,60);
+    fill(225,0,0);
+    ellipse(175,225,60,40);
+    fill(225);
+    ellipse(130,110,40,35);
+    ellipse(215,110,40,10);
+    fill(200,0,0);
+    ellipse(140,115,20,20);
+    ellipse(225,110,17,10);
+    fill(0);
+    triangle(175,95,100,80,250,95);
+    triangle(185,170,175,120,165,170);
+    
+    fill(150,140,180);
+    ellipse(405,150,200,230);
+    triangle(315,100, 310, 160, 290, 70);
+    triangle(520,70,500,160,495,100);
+    noStroke();
+    fill(0);
+    ellipse(405,215,70,60);
+    fill(225,0,0);
+    ellipse(405,225,60,40);
+    fill(225);
+    ellipse(360,110,40,10);
+    ellipse(445,110,40,35);
+    fill(0,0,220);
+    ellipse(350,110,17,10);
+    ellipse(435,115,20,20);
+    fill(0);
+    triangle(405,95,330,95,480,80);
+    triangle(415,170,405,120,395,170);
+  }
